72e8269da4e360700fdb6c02c079a725cd2f3cce
Nikita Liakhovka <n.liakhovka@gmail.com>
2023-06-23 17:02:19 +0900

    cleanup packages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -21,11 +21,11 @@ module.exports = {
       },
       keyframes: {
         fadeIn: {
-          from: { opacity: 1 },
+          from: { opacity: 0 },
           to: { opacity: 1 },
         },
       },
     },
   },
-  plugins: [require('flowbite/plugin'), require('@tailwindcss/container-queries'), require('@tailwindcss/typography')],
+  plugins: [require('flowbite/plugin'),  require('@tailwindcss/typography')],
 };
