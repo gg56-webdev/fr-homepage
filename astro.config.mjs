f6cd9552d9b42b371ffcf7f9abdbedab52b6b66d
Nikita Liakhovka <n.liakhovka@gmail.com>
2023-07-03 15:29:06 +0900

    add home translation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
+import  astroI18next  from 'astro-i18next';
 
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(),astroI18next() ],
   experimental: {
     assets: true,
   },
