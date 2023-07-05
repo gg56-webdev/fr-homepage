78ef3ee4126b2770f117517ea5c4efb740a6e91a
Nikita Liakhovka <n.liakhovka@gmail.com>
2023-07-05 09:59:39 +0900

    add GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
-import  astroI18next  from 'astro-i18next';
-
+import astroI18next from 'astro-i18next';
 import tailwind from '@astrojs/tailwind';
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(),astroI18next() ],
+  integrations: [tailwind(), astroI18next(), partytown({config: {forward: ['dataLayer.push']}})],
   experimental: {
-    assets: true,
-  },
+    assets: true
+  }
 });
