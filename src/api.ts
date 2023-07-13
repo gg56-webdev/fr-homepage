98e2189c70da97a55828dd36309ebc3238658e37
Nikita Liakhovka <n.liakhovka@gmail.com>
2023-07-13 17:41:53 +0900

    update news

--- src/api.ts
+++ src/api.ts
@@ -1,37 +1,48 @@
-import type { WP_REST_API_Post } from 'wp-types';
+import type { WP_REST_API_Post } from "wp-types";
 
-const WP_URL = 'https://gg56.com';
-const WP_POSTS_API_ENDPOINT = 'wp-json/wp/v2/posts'
-const FINGERATE_CATEGORY_ID = '7';
-const ENGLISH_TAG = '107'
-const KOREAN_TAG = '108'
+const WP_URL = "https://gg56.com";
+const WP_POSTS_API_ENDPOINT = "wp-json/wp/v2/posts";
+const FINGERATE_CATEGORY_ID = "7";
+const ENGLISH_TAG = "107";
+const KOREAN_TAG = "108";
 
-export const getPosts = async (lang: 'en' | 'ko' = 'en', minimal = false) => {
+export const getPosts = async (lang: "en" | "ko" = "en", minimal = false) => {
   const wpUrl = new URL(`${WP_URL}/${lang}/${WP_POSTS_API_ENDPOINT}`);
-  wpUrl.searchParams.set('categories', FINGERATE_CATEGORY_ID);
-  wpUrl.searchParams.set('tags', lang === 'en' ? ENGLISH_TAG : KOREAN_TAG);
+  wpUrl.searchParams.set("categories", FINGERATE_CATEGORY_ID);
+  wpUrl.searchParams.set("tags", lang === "en" ? ENGLISH_TAG : KOREAN_TAG);
   if (minimal) {
-    wpUrl.searchParams.set('_fields', 'id,slug');
+    wpUrl.searchParams.set("_fields", "id,slug");
   } else {
-    wpUrl.searchParams.set('_fields', 'id,sticky,title,date,slug,_links.wp:featuredmedia,_links.wp:term');
-    wpUrl.searchParams.set('_embed', 'wp:term,wp:featuredmedia');
+    wpUrl.searchParams.set(
+      "_fields",
+      "id,sticky,title,excerpt,date,slug,_links.wp:featuredmedia,_links.wp:term"
+    );
+    wpUrl.searchParams.set("_embed", "wp:term,wp:featuredmedia");
   }
-
 
   const res = await fetch(wpUrl);
   const posts = (await res.json()) as Pick<
     WP_REST_API_Post,
-    'id' | 'sticky' | 'title' | 'date' | 'slug' | '_embedded'
+    "id" | "sticky" | "title" | "date" | "slug" | "_embedded" | "excerpt"
   >[];
   return posts;
 };
 
-export const getPost = async (id: WP_REST_API_Post['id'], lang: 'en' | 'ko' = 'en') => {
+export const getPost = async (
+  id: WP_REST_API_Post["id"],
+  lang: "en" | "ko" = "en"
+) => {
   const wpUrl = new URL(`${WP_URL}/${lang}/${WP_POSTS_API_ENDPOINT}/${id}`);
-  wpUrl.searchParams.set('_fields', 'content,title,date,_links.wp:featuredmedia,_links.wp:term');
-  wpUrl.searchParams.set('_embed', 'wp:term,wp:featuredmedia');
+  wpUrl.searchParams.set(
+    "_fields",
+    "content,title,date,_links.wp:featuredmedia,_links.wp:term"
+  );
+  wpUrl.searchParams.set("_embed", "wp:term,wp:featuredmedia");
 
   const res = await fetch(wpUrl);
-  const post = (await res.json()) as Pick<WP_REST_API_Post, 'content' | 'title' | 'date' | '_embedded'>;
+  const post = (await res.json()) as Pick<
+    WP_REST_API_Post,
+    "content" | "title" | "date" | "_embedded"
+  >;
   return post;
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,33 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       backgroundImage: ({ theme }) => ({
         brandGradient: `linear-gradient(
           -5deg,
-          ${theme('colors.intelligentBlue/50')},
-          ${theme('colors.intelligentBlue/0')} 50%
+          ${theme("colors.intelligentBlue/50")},
+          ${theme("colors.intelligentBlue/0")} 50%
         ),
-        linear-gradient(225deg,${theme('colors.neonBlue')}, ${theme('colors.neonBlue/0')} 50%),
-        linear-gradient(100deg, ${theme('colors.softFire')} 35%, ${theme('colors.intelligentBlue')});`,
+        linear-gradient(225deg,${theme("colors.neonBlue")}, ${theme(
+          "colors.neonBlue/0"
+        )} 50%),
+        linear-gradient(100deg, ${theme("colors.softFire")} 35%, ${theme(
+          "colors.intelligentBlue"
+        )});`,
       }),
       colors: {
-        intelligentBlue: '#502FC6',
-        softFire: '#CA4450',
-        neonBlue: '#2697C2',
-        deepSpace: '#222134',
-        cyberLilac: '#D8C9D5',
+        intelligentBlue: "#502FC6",
+        softFire: "#CA4450",
+        neonBlue: "#2697C2",
+        deepSpace: "#222134",
+        cyberLilac: "#D8C9D5",
       },
       boxShadow: ({ theme }) => ({
-        center: `0 0 32px 2px ${theme('colors.neonBlue')}`,
+        center: `0 0 32px 2px ${theme("colors.neonBlue")}`,
       }),
       animation: {
-        fadeIn: 'fadeIn 0.5s ease',
-        bob: 'bob 2s ease-in-out infinite',
-        blob: 'blob 8s ease-in-out infinite',
-        stack: 'stack 15s infinite',
-        orbit: 'orbit linear 20s infinite',
+        fadeIn: "fadeIn 0.5s ease",
+        bob: "bob 2s ease-in-out infinite",
+        blob: "blob 8s ease-in-out infinite",
+        stack: "stack 15s infinite",
+        orbit: "orbit linear 20s infinite",
       },
       keyframes: {
         fadeIn: {
@@ -35,60 +39,61 @@ module.exports = {
           to: { opacity: 1 },
         },
         bob: {
-          '50%': { transform: 'translateY(-5%)' },
+          "50%": { transform: "translateY(-5%)" },
         },
         blob: {
-          '33%': { scale: '1.1', translate: '30px -50px' },
-          '66%': { scale: '0.85', translate: '-20px 20px' },
+          "33%": { scale: "1.1", translate: "30px -50px" },
+          "66%": { scale: "0.85", translate: "-20px 20px" },
         },
         stack: {
-          '6.25%': {
-            translate: '0 0%',
+          "6.25%": {
+            translate: "0 0%",
           },
-          '12.5%,18.75%': {
-            translate: '0 -12.5%',
+          "12.5%,18.75%": {
+            translate: "0 -12.5%",
           },
-          '25%,31.25%': {
-            translate: '0 -25%',
+          "25%,31.25%": {
+            translate: "0 -25%",
           },
-          '37.5%,43.75%': {
-            translate: '0 -37.5%',
+          "37.5%,43.75%": {
+            translate: "0 -37.5%",
           },
-          '50%,56.25%': {
-            translate: '0 -50%',
+          "50%,56.25%": {
+            translate: "0 -50%",
           },
-          '62.5%,68.75%': {
-            translate: '0 -62.5%',
+          "62.5%,68.75%": {
+            translate: "0 -62.5%",
           },
-          '75%,81.25%': {
-            translate: '0 -75%',
+          "75%,81.25%": {
+            translate: "0 -75%",
           },
-          '87.5%,100%': {
-            translate: '0 -87.5%',
+          "87.5%,100%": {
+            translate: "0 -87.5%",
           },
         },
         orbit: {
-          to: { '--a': '360deg' },
+          to: { "--a": "360deg" },
         },
       },
     },
   },
   plugins: [
-    require('flowbite/plugin'),
-    require('@tailwindcss/typography'),
+    require("flowbite/plugin"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/container-queries"),
     function ({ addComponents, theme }) {
       addComponents({
-        '.brand-text': {
-          display: ' inline',
-          'background-clip': 'text',
-          '-webkit-background-clip': 'text',
-          'background-image': theme('backgroundImage.brandGradient'),
-          color: ' transparent',
+        ".brand-text": {
+          display: " inline",
+          "background-clip": "text",
+          "-webkit-background-clip": "text",
+          "background-image": theme("backgroundImage.brandGradient"),
+          color: " transparent",
         },
-        '@property --a': {
-          syntax: '<angle>',
-          'initial-value': '0deg',
-          inherits: 'true',
+        "@property --a": {
+          syntax: "<angle>",
+          "initial-value": "0deg",
+          inherits: "true",
         },
       });
     },
