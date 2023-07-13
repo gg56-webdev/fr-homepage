56c9d65d702bcccf602d3dd760a0f3733f2d2b7a
Nikita Liakhovka <n.liakhovka@gmail.com>
2023-07-13 13:50:03 +0900

    work on team

--- src/data/team/exports.ts
+++ src/data/team/exports.ts
@@ -1,2 +1,3 @@
-export { default as founders } from './founders.json';
-export { default as mainTeam } from './main.json';
+export { default as founders } from "./founders.json";
+export { default as mainTeam } from "./main.json";
+export { default as team } from "./team.json";
