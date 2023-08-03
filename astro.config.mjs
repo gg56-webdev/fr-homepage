0546b90878e5c7d0d4dc27ed9f8c359f22116692
Nikita Liakhovka <n.liakhovka@gmail.com>
2023-08-03 17:24:33 +0900

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,30 @@ import { defineConfig } from 'astro/config'
 import astroI18next from 'astro-i18next'
 import tailwind from '@astrojs/tailwind'
 import partytown from '@astrojs/partytown'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.fingerate.world',
   integrations: [
-    tailwind({ applyBaseStyles: false }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
     astroI18next(),
-    partytown({ config: { forward: ['dataLayer.push'] } }),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+    sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en',
+          ko: 'ko',
+        },
+      },
+    }),
   ],
   experimental: {
     assets: true,
