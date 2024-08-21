659f8bbe8b5156ca43cc7950c1a2c06cec0af2bc
Nikita Liakhovka <n.liakhovka@gmail.com>
2024-08-21 13:41:07 +0900

    update nav links

--- src/constants.ts
+++ src/constants.ts
@@ -37,7 +37,8 @@ export const NAV_LINKS = {
   HOW_IT_WORKS: { name: 'How It Works', href: '/how-it-works', isExternal: false, isImportant: false },
   REDEEM_COUPON: { name: 'Redeem a Coupon', href: '/faq#how-to-use-coupon', isExternal: false, isImportant: false },
   BUSINESSES: { name: 'For Businesses', href: 'https://www.gg56.com', isExternal: true, isImportant: false },
-  MISSIONS: { name: 'MISSIONS', href: '/missions', isExternal: false, isImportant: false },
+  MISSIONS: { name: 'Missions', href: '/missions', isExternal: false, isImportant: false },
+  AGENT: { name: 'Agent Mode', href: '/agent', isExternal: false, isImportant: false },
 } as const
 
 export const SOCIAL_LINKS = {
